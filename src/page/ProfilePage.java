f0c8e8fcf698042ad667258ccfa5e6614cd6fb9f
keepgoing-228 <71744036+keepgoing-228@users.noreply.github.com>
2021-06-25 00:16:14 +0800

    update ProfilePage
    add basic information of person

--- src/page/ProfilePage.java
+++ src/page/ProfilePage.java
@@ -1,31 +1,90 @@
-package page;
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 import app.App;
 import myHTTP.Request;
 import myHTTP.Response;
 
-public class ProfilePage extends BasePage implements ActionListener {
-
-	private JButton personalInfoButton = new JButton("ÂÄã‰∫∫Ë≥áÊñô");
-	private JButton uploadedProductButton = new JButton("Â∑≤‰∏äÊû∂ÂïÜÂìÅ");
-	private JButton productUploadButton = new JButton("ÂïÜÂìÅ‰∏äÊû∂");
-	private JButton cartButton = new JButton("Ë≥ºÁâ©Ëªä");
+public class ProfilePage extends BasePage implements ActionListener{
+	private final int WIDTH = 1000;
+	private final int HEIGHT = 800;
+	JLabel personalInfoLabel = new JLabel("∞Ú•ª∏Í∞T");
+	private JButton personalInfoButton = new JButton("≠”§H∏ÍÆ∆");
+	private JButton launchedProductButton = new JButton("§w§W¨[∞”´~");
+	private JButton goOnMarketButton = new JButton("∞”´~§W¨[");
+	private JButton shoppingCartButton = new JButton("¡ ™´®Æ");
+	
 	
 	public ProfilePage() {
+		this.setTitle("ProfilePage");
+//		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//		this.setLayout(new BorderLayout());
 		
-		//Âú®ÂÅ¥ÈÇäÂä†ÂÖ•personalInfo, uploadedProduct, productUpload, cart ButtonÔºåactionListener this
+//		//border
+//		Border rounded = new LineBorder(Color.black, 1, true);
+//		Border empty = new EmptyBorder(300, 3, 0, 0);
+//		Border border = new CompoundBorder(rounded, empty);
 		
+		//panel
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout(new GridBagLayout());
+//		leftPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		JPanel ProfilePanel = new JPanel();
+		ProfilePanel.setLayout(new GridBagLayout());
+		ProfilePanel.setOpaque(true);
+		ProfilePanel.setBorder(BorderFactory.createEtchedBorder());
+		GridBagConstraints gridBag = new GridBagConstraints();
+		gridBag.fill = GridBagConstraints.BOTH;
+		gridBag.gridx = 0;
+		gridBag.gridy = 0;
+		
+		
+		
+		//button
+		ProfilePanel.add(personalInfoLabel,gridBag);
+		personalInfoButton.addActionListener(this);
+		gridBag.gridy = 1;
+		ProfilePanel.add(personalInfoButton,gridBag);
+		launchedProductButton.addActionListener(this);
+		gridBag.gridy = 2;
+		ProfilePanel.add(launchedProductButton,gridBag);
+		goOnMarketButton.addActionListener(this);
+		gridBag.gridy = 3;
+		ProfilePanel.add(goOnMarketButton,gridBag);
+		shoppingCartButton.addActionListener(this);
+		gridBag.gridy = 4;
+		ProfilePanel.add(shoppingCartButton,gridBag);
+		
+		
+		gridBag.gridx = 0;
+		gridBag.gridy = 0;
+		gridBag.fill = GridBagConstraints.BOTH;
+		gridBag.insets = new Insets(0,0,300,0);
+		gridBag.anchor = GridBagConstraints.NORTH;
+		leftPanel.add(ProfilePanel,gridBag);
+		this.add(leftPanel,BorderLayout.WEST);
 	}
-	
-	public void actionPerformed(ActionEvent e) {
 
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
-		case "ÂÄã‰∫∫Ë≥áÊñô": {
+		case "≠”§H∏ÍÆ∆": {
 			try {
 				Response response = App.route(new Request(App.path.PERSONAL_INFO, Request.Method.GET, App.USER, null));
 				PersonalInfoPage page = new PersonalInfoPage(response);
@@ -37,7 +96,7 @@ public class ProfilePage extends BasePage implements ActionListener {
 			}
 			break;
 		}
-		case "Â∑≤‰∏äÊû∂ÂïÜÂìÅ": {
+		case "§w§W¨[∞”´~": {
 			try {
 				Response response = App.route(new Request(App.path.UPLOADED_PRODUCT, Request.Method.GET, App.USER, null));
 				UploadedProductPage page = new UploadedProductPage(response);
@@ -49,7 +108,7 @@ public class ProfilePage extends BasePage implements ActionListener {
 			}
 			break;
 		}
-		case "ÂïÜÂìÅ‰∏äÊû∂": {
+		case "∞”´~§W¨[": {
 			try {
 				Response response = App.route(new Request(App.path.Upload, Request.Method.GET, App.USER, null));
 				UploadPage page = new UploadPage(response);
@@ -61,7 +120,7 @@ public class ProfilePage extends BasePage implements ActionListener {
 			}
 			break;
 		}
-		case "Ë≥ºÁâ©Ëªä": {
+		case "¡ ™´®Æ": {
 			try {
 				Response response = App.route(new Request(App.path.CART, Request.Method.POST, App.USER, null));
 				CartPage page = new CartPage(response);
@@ -75,5 +134,5 @@ public class ProfilePage extends BasePage implements ActionListener {
 		}
 		}
 	}
-
+	
 }
